48b4c6c05522698b68c28113cfbd7d2c00eb0d23
Any Medola <anynhamedola@gmail.com>
2023-05-31 16:29:48 +0200

    Update List.jsx

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,38 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { ListGroup } from 'react-bootstrap';
+import React, { useState } from "react";
+import { ListGroup, Button, Spinner } from "react-bootstrap";
+import "./styles.css";
 
-function List() {
+const List = () => {
   const [repositories, setRepositories] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchRepositories = async () => {
-      setIsLoading(true);
+  const fetchRepositories = async () => {
+    setIsLoading(true);
 
-      try {
-        const response = await fetch('https://api.github.com/users/devpass-tech/repos');
-        const data = await response.json();
-        setRepositories(data);
-      } catch (error) {
-        console.error('Error fetching repositories:', error);
-      }
+    try {
+      const response = await fetch('https://api.github.com/users/devpass-tech/repos');
+      const data = await response.json();
+      setRepositories(data);
+    } catch (error) {
+      console.error('Error fetching repositories:', error);
+    }
 
-      setIsLoading(false);
-    };
+    setIsLoading(false);
+  };
 
-    fetchRepositories();
-  }, []);
+  fetchRepositories();
 
   return (
-    <div>
-      {isLoading}
-      <ListGroup>
-        {repositories.map((repo) => (
-          <ListGroup.Item key={repo.id}>{repo.name}</ListGroup.Item>
-        ))}
-      </ListGroup>
+    <div className="list">
+      <div className="container">
+        <h2 className="title">Devpass Repositories</h2>
+
+        {isLoading ?
+          (<Spinner />) :
+          (
+            <ListGroup className="repositoriesList">
+
+              {repositories.map((repo) => (
+                <ListGroup.Item key={repo.id}>{repo.name}</ListGroup.Item>
+              ))}
+
+            </ListGroup>)}
+        <Button data-testid="button" className="button" variant="primary" onClick={() => fetchRepositories()}>Fetch repositories</Button>
+      </div>
     </div>
   );
-}
+};
 
 export default List;
